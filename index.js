cdd4c72c9263a9a4d833fe2e9a88b21d1c89c70f
Diego Moreira <diego.moreira529@gmail.com>
2022-11-29 23:57:32 -0300

    Separate functions and finish the logic for the inputs

--- index.js
+++ index.js
@@ -24,86 +24,80 @@ function showErrorMsg(input, message) {
   const inputMsg = input.parentElement.querySelector(".input-msg");
   inputMsg.classList.add("error");
   inputMsg.innerText = message;
+  input.setCustomValidity(message);
 }
 
 function removeErrorMsg(input) {
   const inputMsg = input.parentElement.querySelector(".input-msg");
   inputMsg.classList.remove("error");
   inputMsg.innerText = "";
+  input.setCustomValidity("");
+}
+
+// Choose message to appear on #input-msg
+function chooseMessage(e) {
+  const input = e.target;
+  // Special condition for the passwordCheck and bikeBrand
+  if (input.id === "password-check") {
+    if (input.value !== password.value) {
+      showErrorMsg(input, "As senhas estão diferentes!");
+    } else {
+      removeErrorMsg(input);
+    }
+  } else if (input.id === "bike-brand") {
+    if (input.value === "") {
+      showErrorMsg(input, "É necessário escolher a marca!");
+    } else {
+      removeErrorMsg(input);
+    }
+  } else {
+    // Condition for the other inputs
+    // Calls showMessage function with the respective message
+    if (input.validity.patternMismatch) {
+      switch (input.id) {
+        case "first-name":
+          showErrorMsg(
+            input,
+            `Há caracteres inválidos neste campo! Apenas letras e os caracteres , . ' -  são permitidos.`
+          );
+          break;
+        case "last-name":
+          showErrorMsg(
+            input,
+            `Há caracteres inválidos neste campo! Apenas letras e os caracteres , . ' -  são permitidos.`
+          );
+          break;
+        case "e-mail":
+          showErrorMsg(
+            input,
+            `E-mail inválido, favor verificar! Exemplo de e-mail válido: joaosilva@gmail.com`
+          );
+          break;
+        case "phone":
+          showErrorMsg(
+            input,
+            `Telefone inválido, favor verificar! Deve conter 11 dígitos (DDD + 9 dígitos), apenas números.`
+          );
+          break;
+        case "cep":
+          showErrorMsg(
+            input,
+            `CEP inválido, favor verificar! Deve conter 8 dígitos, apenas números.`
+          );
+          break;
+        case "password":
+          showErrorMsg(
+            input,
+            `Senha não permitida. Deve conter de 8 a 10 caracteres.`
+          );
+          break;
+      }
+    } else {
+      removeErrorMsg(input);
+    }
+  }
 }
 
 allInputs.forEach((input) => {
-  input.addEventListener("input", (e) => {
-    // Special condition for the passwordCheck
-    if (e.target.id === "password-check") {
-      if (e.target.value !== password.value) {
-        console.log(e.target.value !== password.value);
-        showErrorMsg(e.target, "As senhas estão diferentes!");
-      } else {
-        removeErrorMsg(e.target);
-      }
-    } else {
-      // Condition for the other inputs
-      // Calls showMessage function with the respective message
-      if (e.target.validity.patternMismatch) {
-        switch (e.target.id) {
-          case "first-name":
-            showErrorMsg(
-              e.target,
-              `Há caracteres inválidos neste campo! Apenas letras e os 
-            caracteres , . ' -  são permitidos.`
-            );
-            break;
-          case "last-name":
-            showErrorMsg(
-              e.target,
-              `Há caracteres inválidos neste campo! Apenas letras e os 
-            caracteres , . ' -  são permitidos.`
-            );
-            break;
-          case "e-mail":
-            showErrorMsg(
-              e.target,
-              `E-mail inválido, favor verificar! Exemplo de e-mail válido: 
-            joaosilva@gmail.com`
-            );
-            break;
-          case "phone":
-            showErrorMsg(
-              e.target,
-              `Telefone inválido, favor verificar! Deve conter 11 dígitos 
-            (DDD + 9 dígitos), apenas números.`
-            );
-            break;
-          case "cep":
-            showErrorMsg(
-              e.target,
-              `CEP inválido, favor verificar! Deve conter 8 dígitos, apenas 
-            números.`
-            );
-            break;
-          case "password":
-            showErrorMsg(
-              e.target,
-              `Senha não permitida. Deve conter de 8 a 10 caracteres.`
-            );
-            break;
-          case "bike-brand":
-            showErrorMsg(e.target, ``);
-            break;
-        }
-      } else {
-        removeErrorMsg(e.target);
-      }
-    }
-  });
+  input.addEventListener("input", chooseMessage);
 });
-
-// Validation for first and last name
-// [fName, lName].forEach((input) => {
-//   input.addEventListener("input", (e) => {
-//     if (e.target.validity.patternMismatch)
-//       showErrorMsg(e.target, "Há caracteres inválidos neste campo.");
-//     else removeErrorMsg(e.target);
-//   });
-// });
